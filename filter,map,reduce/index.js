6d2a5581e961bd0084750ed67f84b1d3057886d7
E25llin <143622878+E25llin@users.noreply.github.com>
2024-10-22 11:59:35 -0300

    Create index.js

---
+++ filter,map,reduce/index.js
@@ -0,0 +1,9 @@
+const produtos = [
+    {nome:"notebook", preco:6000.00,eletronico:true},
+    {nome:"computador", preco:4500.00,eletronico:true},
+    {nome:"teclado", preco:100.00,eletronico:false},
+    {nome:"mouser", preco:75.00,eletronico:true},
+    {nome:"cadeira gamer", preco:1775.00,eletronico:false}
+]
+const e =(vetor)=>!vetor.eletronico
+const
